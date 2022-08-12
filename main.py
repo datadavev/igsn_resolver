ed9695f405e39f04035ba749c9414a9e15b7b4be
datadavev <605409+datadavev@users.noreply.github.com>
2022-08-12 09:51:54 -0400

    feat: Added documentation

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 """
+A basic IGSN resolver implementation.
+
+This service receives an IGSN identifier and looks up the target
+address using the [hdl.handle.net API](http://www.handle.net/proxy_servlet.html),
+then either redirects the client to the target or presents
+basic metadata about the identifier.
+
 Run me like: `uvicorn main:app --reload`
 """
+import logging
 import typing
 import fastapi
 import fastapi.responses
 import fastapi.middleware.cors
 import igsnresolve
 
+L = logging.getLogger("resolver")
 
-app = fastapi.FastAPI()
+app = fastapi.FastAPI(
+    title="IGSN Resolver",
+    description=__doc__,
+    version="0.1.0",
+    contact={
+        "name":"Dave Vieglais",
+        "url": "https://github.com/datadavev/"
+    },
+    license_info={
+        "name":"Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html"
+    }
+)
 app.add_middleware(
     fastapi.middleware.cors.CORSMiddleware,
     allow_origins=[
@@ -39,12 +60,19 @@ async def igsn_info(
 
 @app.get("/{igsn}")
 async def resolve(
-    igsn: str, accept: typing.Union[str, None] = fastapi.Header(default=None)
+    igsn: str, request: fastapi.Request, accept: typing.Union[str, None] = fastapi.Header(default=None)
 ):
-    info = await igsnresolve.resolveIgsn(igsn)
-    if info.target is not None:
-        return fastapi.responses.RedirectResponse(info.target)
-    raise fastapi.HTTPException(status_code=404, detail=dict(info))
+    try:
+        info = await igsnresolve.resolveIgsn(igsn)
+        if info.target is not None:
+            headers = {
+                "Link": f'</info/{info.normalized}>; rel="alternate"'
+            }
+            return fastapi.responses.RedirectResponse(info.target, headers=headers)
+        raise fastapi.HTTPException(status_code=404, detail=dict(info))
+    except Exception as e:
+        L.exception(e)
+        raise fastapi.HTTPException(status_code=500, detail=str(e))
 
 
 @app.get("/")
